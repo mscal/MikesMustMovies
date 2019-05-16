748e15076692741b9aa9087a69fc8b7718cccd3e
Mike <mscal4700@gmail.com>
2019-05-17 04:18:59 +1000

    Update to App.js / css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Tabletop from 'tabletop';
+
+
+function MovieGallery({data}) {
+  return <div className="movie-gallery">
+    {
+      data.map(obj => {
+        return (
+            <div className="movie-obj" key={obj.movie}>
+              <p>{obj.movie}</p>
+              <p>{obj.dateofrelease}</p>
+              <img alt={obj.movie + " poster"} src={obj.img} />
+            </div>
+          )
+      })
+    }
+  </div>;
+}
+
+function SheetConnect(setData) {
+  Tabletop.init({
+    key: '1I-VdIEjcSPe8LYGsVAp-P8Nk6QCEdWGVtXkKeFO-U-4',
+    callback: googleData => {
+    setData(
+        googleData
+    )
+    },
+    simpleSheet: true
+  })
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    SheetConnect(setData);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Mike's Must Movies</h1>
+          <h5 className="App-title">A Google sheet connected project</h5>
+        </header>
+        <MovieGallery  data={data}/>
+      </div>
+    );
 }
 
 export default App;
